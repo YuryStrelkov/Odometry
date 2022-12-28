c0c895877a0e528f5aa2643298e374c1c7de48fb
Daniil Khoroshev <daniil.khoroshev@netcracker.com>
2022-12-28 22:15:12 +0400

    added binocular calibration

--- devices/cameras/camera_cv.py
+++ devices/cameras/camera_cv.py
@@ -1,5 +1,5 @@
 import datetime as datetime
-from os import mkdir, listdir, path
+from os import mkdir, listdir, path, makedirs
 import numpy as np
 import cv2 as cv
 import time
@@ -301,6 +301,8 @@ class CameraCV:  # (Camera):
         img_names = listdir(chess_board_img_dir)
         if len(img_names) == 0:
             raise RuntimeError("chess board images folder are empty!")
+        if chess_board_size is None:
+            raise RuntimeError("chess board suze is None!")
 
         # array that contains abs paths of all the images
         images = [path.join(chess_board_img_dir, img_name) for img_name in img_names]
@@ -342,7 +344,7 @@ class CameraCV:  # (Camera):
         #### CALIBRATION ####
 
         ret, camera_matrix, dist, r_vecs, t_vecs = cv.calibrateCamera(obj_points_arr, img_points_arr, frame_size,
-                                                                    None, None)
+                                                                      None, None)
         print("Camera Calibrated: ", ret)
         print("\nCamera Matrix:\n", camera_matrix)
         print("\nDistortion Parameters:\n", dist)
@@ -380,16 +382,15 @@ class CameraCV:  # (Camera):
         print(f"\ntotal error: {mean_error}")
         print('\n\n')
 
-
     @staticmethod
-    def binocular_camera_calibration(stereo_images_dir: str = None):
+    def get_stereo_images_for_calibration(stereo_images_dir: str = None):
         stereo_images_dir = stereo_images_dir if stereo_images_dir is not None else "./binocular_calibration"
         image_left_dir = path.join(stereo_images_dir, "stereo_left/image_left")
         image_right_dir = path.join(stereo_images_dir, "stereo_right/image_right")
         try:
             for directory in [stereo_images_dir, image_left_dir, image_right_dir]:
                 if not path.exists(directory):
-                    mkdir(directory)
+                    makedirs(directory)
 
             cap1 = cv.VideoCapture(0)
             cap2 = cv.VideoCapture(1)
@@ -403,8 +404,8 @@ class CameraCV:  # (Camera):
                 if k == 27:
                     break
                 elif k == ord('s'):
-                    cv.imwrite(path.join(image_left_dir), f"{num}.png", img1)
-                    cv.imwrite(path.join(image_left_dir), f"{num}.png", img2)
+                    cv.imwrite(path.join(image_left_dir, f"{num}.png"), img1)
+                    cv.imwrite(path.join(image_right_dir, f"{num}.png"), img2)
                     print('images saved!')
                     num += 1
                 cv.imshow('Img 1', img1)
@@ -413,15 +414,151 @@ class CameraCV:  # (Camera):
             print(ex)
             exit(-1)
 
+    @staticmethod
+    def binocular_calibration(stereo_images_dir: str = None, chess_board_size: tuple = None):
+        stereo_images_dir = stereo_images_dir if stereo_images_dir is not None else "./binocular_calibration"
+        image_left_dir = path.join(stereo_images_dir, "stereo_left/image_left")
+        image_right_dir = path.join(stereo_images_dir, "stereo_right/image_right")
+
+        for directory in [stereo_images_dir, image_left_dir, image_right_dir]:
+            if not path.exists(directory):
+                raise RuntimeError(f"{directory} doesn't exist!")
+
+        if chess_board_size is None:
+            raise RuntimeError(f"chess board size is None!")
+
+        # getting all the images names
+        img_left_names = listdir(image_left_dir)
+        img_right_names = listdir(image_right_dir)
+        # array that contains abs paths of all the images
+        images_left = [path.join(image_left_dir, img_name) for img_name in img_left_names]
+        images_right = [path.join(image_right_dir, img_name) for img_name in img_right_names]
+
+        # getting images width and height
+        img_l = cv.imread(images_left[0])
+        frame_size_l = (img_l.shape[1], img_l.shape[0])
+        img_r = cv.imread(images_right[0])
+        frame_size_r = (img_r.shape[1], img_r.shape[0])
+        if frame_size_l != frame_size_r:
+            raise RuntimeError(f"size of images are not the same!")
+        frame_size = frame_size_l
+
+        print(f'{chess_board_size=}')
+        print(f'{frame_size=}')
+
+        # termination criteria
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ..., (6,5,0)
+        obj_points = np.zeros((chess_board_size[0] * chess_board_size[1], 3), np.float32)
+        obj_points[:, :2] = np.mgrid[0:chess_board_size[0], 0:chess_board_size[1]].T.reshape(-1, 2)
+
+        # arrays to store object points and image points from all the images
+        obj_points_arr = []  # 3d point in real world space
+        img_points_arr_l = []  # 2d point in image plane
+        img_points_arr_r = []  # 2d point in image plane
+
+        for img_left, img_right in zip(images_left, images_right):
+            img_l = cv.imread(img_left)
+            img_r = cv.imread(img_right)
+            gray_l = cv.cvtColor(img_l, cv.COLOR_BGR2GRAY)
+            gray_r = cv.cvtColor(img_r, cv.COLOR_BGR2GRAY)
+
+            # cv.imshow("gray l", gray_l)
+            # cv.imshow("gray r", gray_r)
+            # cv.waitKey(1000)
+
+            # find the chess board corners
+            ret_l, corners_l = cv.findChessboardCorners(gray_l, chess_board_size, None)
+            ret_r, corners_r = cv.findChessboardCorners(gray_r, chess_board_size, None)
+
+            # if found, add object points, image point (after refining them)
+            if ret_l is True and ret_r is True:
+                obj_points_arr.append(obj_points)
+
+                corners_l = cv.cornerSubPix(gray_l, corners_l, (11, 11), (-1, -1), criteria)
+                img_points_arr_l.append(corners_l)
+                corners_r = cv.cornerSubPix(gray_l, corners_r, (11, 11), (-1, -1), criteria)
+                img_points_arr_r.append(corners_r)
+
+                # draw and display the corners
+                cv.drawChessboardCorners(img_l, chess_board_size, corners_l, ret_l)
+                cv.imshow('img left', img_l)
+                cv.drawChessboardCorners(img_r, chess_board_size, corners_r, ret_r)
+                cv.imshow('img left', img_r)
+                cv.waitKey(1000)
+            cv.destroyAllWindows()
+
+            #### CALIBRATION ####
+
+            ret_l, camera_matrix_l, dist_l, r_vecs_l, t_vecs_l = cv.calibrateCamera(obj_points_arr, img_points_arr_l,
+                                                                                    frame_size, None, None)
+            h_l, w_l, channels_l = img_l.shape
+            new_camera_matrix_l, roi_l = cv.getOptimalNewCameraMatrix(camera_matrix_l, dist_l, (w_l, h_l), 1, (w_l, h_l))
+
+            ret_r, camera_matrix_r, dist_r, r_vecs_r, t_vecs_r = cv.calibrateCamera(obj_points_arr, img_points_arr_r,
+                                                                                    frame_size, None, None)
+            h_r, w_r, channels_r = img_r.shape
+            new_camera_matrix_r, roi_r = cv.getOptimalNewCameraMatrix(camera_matrix_r, dist_r, (w_r, h_r), 1, (w_r, h_r))
+
+            #### Stereo Vision Calibration
+
+            flags = 0
+            flags |= cv.CALIB_FIX_INTRINSIC
+
+            criteria_stereo = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+            ret_stereo, new_camera_matrix_l, dist_l, new_camera_matrix_r, dist_r, rot, trans, essential_matrix, \
+            fundamental_matrix = cv.stereoCalibrate(
+                obj_points_arr, img_points_arr_l, img_points_arr_r,
+                new_camera_matrix_l, dist_l,
+                new_camera_matrix_r, dist_r,
+                gray_l.shape[::-1], criteria_stereo, flags)
+
+            ##### Stereo Rectification #####
+
+            rectify_scale = 1
+            rect_l, rect_r, proj_matrix_l, proj_matrix_r, Q, roi_l, roi_r = cv.stereoRectify(new_camera_matrix_l,
+                                                                                             dist_l,
+                                                                                             new_camera_matrix_r,
+                                                                                             dist_r,
+                                                                                             gray_l.shape[::-1], rot,
+                                                                                             trans,
+                                                                                             rectify_scale, (0, 0))
+
+            print(f'{dist_l=}\n{dist_r=}\n')
+            print(f'{rot=}\n{trans=}\n')
+
+            stereo_map_l = cv.initUndistortRectifyMap(new_camera_matrix_l, dist_l, rect_l, proj_matrix_l,
+                                                      gray_l.shape[::-1],
+                                                      cv.CV_16SC2)
+            stereo_map_r = cv.initUndistortRectifyMap(new_camera_matrix_l, dist_r, rect_r, proj_matrix_r,
+                                                      gray_r.shape[::-1],
+                                                      cv.CV_16SC2)
+
+            print("Saving parameters!")
+            cv_file = cv.FileStorage('stereo_map.xml', cv.FILE_STORAGE_WRITE)
+
+            cv_file.write('stereo_map_l_x', stereo_map_l[0])
+            cv_file.write('stereo_map_l_y', stereo_map_l[1])
+            cv_file.write('stereo_map_r_x', stereo_map_r[0])
+            cv_file.write('stereo_map_r_y', stereo_map_r[1])
+
+            cv_file.release()
+
 
 def camera_cv_test():
     # cam = CameraCV()
     # cam.show_video()
     # cam.record_frames()
-    img_folder = 'C:/Users/daniil/PycharmProjects/Odometry/devices/cameras/chess_board_images'
-    chess_board_size = (15, 9)
 
-    CameraCV.monocular_camera_calibration(chess_board_img_dir=img_folder, chess_board_size=chess_board_size)
+    # img_folder = 'C:/Users/daniil/PycharmProjects/Odometry/devices/cameras/chess_board_images'
+    chess_board_size = (15, 9)
+    # CameraCV.monocular_camera_calibration(chess_board_img_dir=img_folder, chess_board_size=chess_board_size)
+
+    stereo_img_folder = 'C:/Users/dakh0322/PycharmProjects/Odometry/devices/cameras/binocular_calibration'
+    # CameraCV.get_stereo_images_for_calibration(stereo_img_folder)
+    CameraCV.binocular_calibration(stereo_img_folder, chess_board_size)
+    # CameraCV.monocular_camera_calibration(stereo_img_folder + "/stereo_right/image_right", chess_board_size)
 
 
 if __name__ == "__main__":
